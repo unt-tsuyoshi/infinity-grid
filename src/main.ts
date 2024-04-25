dc0a010eedf1cfc053646d1f708abd255690205e
tsuyoshi takeuchi <t-takeuchi@un-t.com>
2024-04-25 16:36:26 +0900

    無限グリッドの機能を実装

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,8 @@
 import * as THREE from "three";
-type Card = {
-  mesh: THREE.Mesh<
-    THREE.PlaneGeometry,
-    THREE.MeshBasicMaterial,
-    THREE.Object3DEventMap
-  >;
-  id: number;
-};
 
 type ScrollParam = {
   vec: THREE.Vector2;
   total: THREE.Vector2;
-  gridPos: THREE.Vector2;
-  dir: "left" | "right" | "bottom" | "top";
-};
-
-type Edge = {
-  left: number;
-  right: number;
-  top: number;
-  bottom: number;
 };
 
 const initWidth = window.innerWidth;
@@ -30,9 +13,16 @@ const camera = new THREE.PerspectiveCamera(45, initWidth / initHeight);
 const gap = 40; // カード同士の間隔
 const cardWidth = 300; // カードの横幅
 const cardHeight = 400; // カードの縦幅
-const cards: Array<Card> = [];
-const grids: THREE.Group<THREE.Object3DEventMap> = new THREE.Group();
-const grid: THREE.Group<THREE.Object3DEventMap> = new THREE.Group();
+const cards: Array<
+  THREE.Mesh<
+    THREE.PlaneGeometry,
+    THREE.MeshBasicMaterial,
+    THREE.Object3DEventMap
+  >
+> = [];
+
+const baseGrid: THREE.Group<THREE.Object3DEventMap> = new THREE.Group();
+const grids: Array<THREE.Group<THREE.Object3DEventMap>> = [];
 let gridWidth: number;
 let gridHeight: number;
 let isDrag = false;
@@ -41,15 +31,6 @@ const damping = 0.9;
 const scrollParam: ScrollParam = {
   vec: new THREE.Vector2(),
   total: new THREE.Vector2(),
-  gridPos: new THREE.Vector2(),
-  dir: "right",
-};
-
-const edge: Edge = {
-  left: 0,
-  right: 0,
-  top: 0,
-  bottom: 0,
 };
 
 const previousMousePosition = { x: 0, y: 0 };
@@ -77,7 +58,7 @@ const createCards = () => {
       const plane = new THREE.Mesh(geometry, material);
       plane.position.x = col * (cardWidth + gap) + gap / 2;
       plane.position.y = row * (cardHeight + gap) + gap / 2;
-      cards.push({ mesh: plane, id });
+      cards.push(plane);
       id++;
     }
   }
@@ -85,22 +66,111 @@ const createCards = () => {
   gridHeight = rows * (cardHeight + gap) - gap;
   camera.position.x = (cols * (cardWidth + gap) - cardWidth) / 2;
   camera.position.y = (rows * (cardHeight + gap) - cardHeight) / 2;
-  grid.userData.width = gridWidth;
-  grid.userData.height = gridHeight;
+  baseGrid.userData.width = gridWidth;
+  baseGrid.userData.height = gridHeight;
+
   cards.map((card) => {
-    grid.add(card.mesh);
+    baseGrid.add(card);
   });
-  grids.add(grid);
-  scene.add(grid);
-  scrollParam.total.x = (cols * (cardWidth + gap) - cardWidth) / 2;
-  scrollParam.total.y = (rows * (cardHeight + gap) - cardHeight) / 2;
+  const clone = baseGrid.clone();
+  grids.push(clone);
+  scene.add(clone);
 };
 
-const checkViewport = () => {};
+const addGroup = () => {
+  const fov = camera.fov * (Math.PI / 180);
+  const height = 2 * Math.tan(fov / 2) * camera.position.z;
+  const width = height * camera.aspect;
+  const hBase = (gridWidth - width) / 2;
+  const vBase = (gridHeight - width) / 2;
+  grids.map((grid) => {
+    const leftEdge = -hBase + grid.position.x;
+    const rightEdge = hBase + grid.position.x;
+    const topEdge = vBase + grid.position.y;
+    const bottomEdge = -vBase + grid.position.y;
+    const isOverLeft = leftEdge > scrollParam.total.x;
+    const isOverRight = rightEdge < scrollParam.total.x;
+    const isOverTop = topEdge < scrollParam.total.y;
+    const isOverBottom = bottomEdge > scrollParam.total.y;
+    if (isOverLeft) {
+      const x = grid.position.x - (gap + gridWidth);
+      const y = grid.position.y;
+      const sameXGrids = grids.filter((grid) => {
+        return grid.position.x === x;
+      });
+      const isExist = sameXGrids.some((grid) => {
+        return grid.position.y === y;
+      });
+      if (!isExist) {
+        const clone = baseGrid.clone();
+        clone.position.y = grid.position.y;
+        clone.position.x = x;
+        grids.push(clone);
+        scene.add(clone);
+      }
+    }
+    if (isOverRight) {
+      const x = grid.position.x + (gap + gridWidth);
+      const y = grid.position.y;
+      const sameXGrids = grids.filter((grid) => {
+        return grid.position.x === x;
+      });
+      const isExist = sameXGrids.some((grid) => {
+        return grid.position.y === y;
+      });
+      if (!isExist) {
+        const clone = baseGrid.clone();
+        clone.position.y = grid.position.y;
+        clone.position.x = x;
+        console.log("right");
+        grids.push(clone);
+        scene.add(clone);
+      }
+    }
+    if (isOverTop) {
+      const x = grid.position.x;
+      const y = grid.position.y + (gap + gridHeight);
+      const sameYGrids = grids.filter((grid) => {
+        return grid.position.y === y;
+      });
+      const isExist = sameYGrids.some((grid) => {
+        return grid.position.x === x;
+      });
+      if (!isExist) {
+        const clone = baseGrid.clone();
+        clone.position.x = grid.position.x;
+        clone.position.y = y;
+        console.log("top");
+        grids.push(clone);
+        scene.add(clone);
+      }
+    }
+    if (isOverBottom) {
+      const x = grid.position.x;
+      const y = grid.position.y - (gap + gridHeight);
+      const sameYGrids = grids.filter((grid) => {
+        return grid.position.y === y;
+      });
+      const isExist = sameYGrids.some((grid) => {
+        return grid.position.x === x;
+      });
+      if (!isExist) {
+        const clone = baseGrid.clone();
+        clone.position.x = grid.position.x;
+        clone.position.y = y;
+        grids.push(clone);
+        scene.add(clone);
+      }
+    }
+  });
+};
 
-const setPosition = () => {};
-
-const addGroup = () => {};
+const rotateCard = (mouseX: number, mouseY: number) => {
+  const rotationX = ((mouseX / window.innerWidth) * Math.PI) / 2;
+  cards.map((card) => {
+    card.rotation.x = rotationX;
+  });
+};
 
 const onMouseDown = (e: MouseEvent) => {
   isDrag = true;
@@ -116,6 +186,7 @@ const onMouseUp = (e: MouseEvent) => {
 };
 
 const onMouseMove = (e: MouseEvent) => {
+  rotateCard(e.clientX, e.clientY);
   if (!isDrag) return;
   const deltaX = e.clientX - previousMousePosition.x;
   const deltaY = -(e.clientY - previousMousePosition.y);
@@ -146,8 +217,8 @@ const tick = () => {
   if (Math.abs(scrollParam.vec.y) < 0.05) scrollParam.vec.y = 0;
   camera.position.x = camera.position.x - scrollParam.vec.x;
   camera.position.y = camera.position.y - scrollParam.vec.y;
-  scrollParam.total.x += scrollParam.vec.x;
-  scrollParam.total.y += scrollParam.vec.y;
+  scrollParam.total.x -= scrollParam.vec.x;
+  scrollParam.total.y -= scrollParam.vec.y;
   addGroup();
   renderer.render(scene, camera);
   requestAnimationFrame(tick);
